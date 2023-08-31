fb1264ff05c93a7eec757f26c44bb8683ab01ddf
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-08-31 12:50:53 +0400

    fluentvaldation

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeCommandValidator : AbstractValidator<CreateLeaveTypeCommand>
+{
+    public CreateLeaveTypeCommandValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .NotNull()
+            .MaximumLength(70).WithMessage("PropertyName} must be fewer than 70.");
+
+        RuleFor(p => p.DefautlDays)
+            .NotEmpty().WithMessage("{ProperyName} is required.")
+            .NotNull()
+
+    }
+}
--- HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -7,29 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery,
+    List<LeaveTypeDto>>
 {
-    public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery,
-        List<LeaveTypeDto>>
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
     {
-        private readonly IMapper _mapper;
-        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        this._mapper = mapper;
+        this._leaveTypeRepository = leaveTypeRepository;
+    }
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        //Query the database
+        var leaveTypes = await _leaveTypeRepository.GetAsync();
 
-        public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
-        {
-            this._mapper = mapper;
-            this._leaveTypeRepository = leaveTypeRepository;
-        }
-        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
-        {
-            //Query the database
-            var leaveTypes = await _leaveTypeRepository.GetAsync();
+        //Convert data objects to DTO objects
+        var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
 
-            //Convert data objects to DTO objects
-            var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
-
-            //return list of DTO object
-            return data;
-        }
+        //return list of DTO object
+        return data;
     }
 }
