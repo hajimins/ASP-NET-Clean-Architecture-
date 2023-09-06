8889bb3b2d1b33c0ab7a697ebec2b282818bda3e
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-09-06 18:36:22 +0400

    adding migration

---
+++ HR.LeaveManagement.Persistence/Migrations/20230906142542_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2023, 9, 6, 18, 25, 42, 655, DateTimeKind.Local).AddTicks(9728), new DateTime(2023, 9, 6, 18, 25, 42, 655, DateTimeKind.Local).AddTicks(9742) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2023, 9, 6, 18, 19, 10, 616, DateTimeKind.Local).AddTicks(3079), new DateTime(2023, 9, 6, 18, 19, 10, 616, DateTimeKind.Local).AddTicks(3095) });
+        }
+    }
+}
