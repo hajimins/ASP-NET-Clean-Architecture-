30daed0d97b30d296fd2ec58e7a68739ed0d926d
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-09-13 18:16:10 +0400

    GetLeaveAllocationListHandler

--- HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/GetLeaveAllocationListHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/GetLeaveAllocationListHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,20 @@ namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLea
     public class GetLeaveAllocationListHandler : IRequestHandler<GetLeaveAllocationListQuery,
         List<LeaveAllocationDto>>
     {
-        public Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationListQuery request, CancellationToken cancellationToken)
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+        public GetLeaveAllocationListHandler(ILeaveAllocationRepository leaveAllocationRepository,
+            IMapper mapper)
         {
-            throw new NotImplementedException();
+            this._leaveAllocationRepository = leaveAllocationRepository;
+            this._mapper = mapper;
+        }
+        public async Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationListQuery request, CancellationToken cancellationToken)
+        {
+            var leaveAllocations = await _leaveAllocationRepository.GetLeaveAllocationsWithDetails();
+            var allocations = _mapper.Map<List<LeaveAllocationDto>>(leaveAllocations);
+
+            return allocations;
         }
     }
 }
